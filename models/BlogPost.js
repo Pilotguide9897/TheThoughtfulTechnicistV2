dfc34dc4d0dd59c60db0eea1fa23e53e8a7dc1de
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-21 11:35:50 -0300

    added models and main.handlebars

---
+++ models/BlogPost.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creator_username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true, 
+    modelName: "BlogPosts",
+  }
+);
+
+module.exports = BlogPost;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const BlogPost = require('./BlogPost');
+const Bloggers = require('./Bloggers');
+
+module.exports = {
+    BlogPost,
+    Bloggers,
+};
