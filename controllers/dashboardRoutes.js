057cf9c53503d07c4438a7e8254333202e1fad63
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-22 15:25:21 -0300

    commit before switching to post branch

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,41 +3,41 @@ const { BlogPost, Bloggers } = require("../models");
 const Blogger = require("../models/Bloggers");
 const hasAuthorization = require("../utils/authorize");
 
-// router.get("/", hasAuthorization, async (req, res) => {
-//   try {
-//     const userId = 
-//     const postData = await BlogPost.findAll({
-//       where: {
-//         creator_id: userId, // Add this line to filter posts by the user's ID
-//       },
-//     });
-
-//     const blogpostData = postData.map((blogPost) =>
-//       blogPost.get({ plain: true })
-//     );
-
-//     res.render("dashboard", {
-//       blogpostData,
-//     });
-
-//     res.render("dashboard");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-router.get("/", async (req, res) => {
+router.get("/", hasAuthorization, async (req, res) => {
   try {
-    const data = {logged_in: req.session.logged_in}
-    console.log("Data passed to Handlebars:", data);
-    console.log("Dashboard session data:", req.session);
+    const userId = req.session.user_id; // Get the user ID from the session
+
+    const postData = await BlogPost.findAll({
+      where: {
+        creator_id: userId, // Filter posts by the user's ID
+      },
+    });
+
+    const blogpostData = postData.map((blogPost) =>
+      blogPost.get({ plain: true })
+    );
 
     res.render("dashboard", {
-      logged_in: req.session.logged_in,
+      blogpostData,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const data = {logged_in: req.session.logged_in}
+//     console.log("Data passed to Handlebars:", data);
+//     console.log("Dashboard session data:", req.session);
+
+//     res.render("dashboard", {
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
