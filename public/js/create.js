17f5269e5c1dd575e746457b96f0c13f6c66a8e3
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-22 16:49:16 -0300

    able to route to a create post page

---
+++ public/js/create.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".create-post").on("click", function () {
+    window.location.href = "/create";
+  });
+});
+
+// document.getElemenntById("create-post").addEventListener('click', displayText);
+
+// let displayText = () => {
+//     console.log('this click registered');
+// }
